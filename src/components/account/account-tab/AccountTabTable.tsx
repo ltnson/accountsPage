fcfa0743346ff6f4803ce0671dec31e8889eaba6
SonLe <sonltnfx17838@funix.edu.vn>
2023-07-06 11:31:55 +0700

    format

--- src/components/account/account-tab/AccountTabTable.tsx
+++ src/components/account/account-tab/AccountTabTable.tsx
@@ -98,6 +98,82 @@ const AccountTabTable = () => {
         <AccountFilter />
       </div>
     </div>
+
+    /////
+    // <div className="w-full tab-table ">
+    //   <Toaster />
+    //   <Typography
+    //     component="div"
+    //     sx={{
+    //       '@media (min-width: 1280px)': {
+    //         width: '100%',
+    //         display: 'grid',
+    //         gridTemplateColumns: '1fr 320px',
+    //         gridTemplateRows: 'auto auto',
+    //         gridTemplateAreas:
+    //           "'header  sidebar' 'main  sidebar' 'main  sidebar'",
+    //       },
+    //     }}
+    //   >
+    //     <Typography
+    //       component="div"
+    //       sx={{
+    //         '@media (min-width: 1280px)': { gridArea: 'header' },
+    //       }}
+    //     >
+    //       <div className="grow-0 sm:py-[28px] py-3 flex flex-wrap ">
+    //         <TextField
+    //           placeholder="Search"
+    //           className="search-account"
+    //           size="small"
+    //           onChange={(e) => handleToSearch(e.target.value)}
+    //           InputProps={{
+    //             startAdornment: (
+    //               <InputAdornment position="start">
+    //                 <SearchSVG />
+    //               </InputAdornment>
+    //             ),
+    //           }}
+    //         />
+    //         <TabHeadFilter />
+    //       </div>
+    //     </Typography>
+    //     <Typography
+    //       component="div"
+    //       sx={{ '@media (min-width: 1280px)': { gridArea: 'sidebar' } }}
+    //     >
+    //       <div
+    //         className={`transition-width duration-300 ease-in-out ${
+    //           showArr.filter
+    //             ? 'w-full h-auto xl:w-80 xl:pl-3 pt-7'
+    //             : 'w-0 h-0 transition-width duration-300 ease-in-out'
+    //         }`}
+    //       >
+    //         <AccountFilter />
+    //       </div>
+    //     </Typography>
+    //     <Typography
+    //       component="div"
+    //       sx={{
+    //         '@media (min-width: 1280px)': { gridArea: 'main' },
+    //       }}
+    //     >
+    //       {data?.users && (
+    //         <TableContainer className="scroll-style">
+    //           <Table border={1}>
+    //             <HeadTab />
+    //             <BodyTab accountsArray={data.users} />
+    //           </Table>
+    //         </TableContainer>
+    //       )}
+    //       {isLoading && (
+    //         <div className="w-full p-8 h-full flex justify-center items-center">
+    //           <CircularProgress size="lg" />
+    //         </div>
+    //       )}
+    //     </Typography>
+    //   </Typography>
+    // </div>
   );
 };
 
